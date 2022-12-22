15ce86de2938c25c8a8a0dbd5f10fb542d0e6dd0
Nate Tyalor <taylonr@gmail.com>
2022-12-22 08:49:09 -0600

    Full promise chain

--- public/orders.js
+++ public/orders.js
@@ -1,23 +1,23 @@
 let statuses = [];
-let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://localhost:3000/api/orderStatuses");
-xhr.onload = () => {
-  statuses = JSON.parse(xhr.responseText);
+showWaiting();
 
-  let xhr2 = new XMLHttpRequest();
-  xhr2.open("GET", "http://localhost:3000/api/orders");
-  xhr2.onload = () => {
-    const orders = JSON.parse(xhr2.responseText);
-    const fullOrders = orders.map((o) => {
-      o.orderStatus = statuses.find(
-        (s) => s.id === o.orderStatusId
-      ).description;
-      return o;
+axios
+  .get("http://localhost:3000/api/orderStatuses")
+  .then(({ data }) => {
+    statuses = data;
+    return axios.get("http://localhost:3000/api/orders");
+  })
+  .then(({ data }) => {
+    let orders = data.map((o) => {
+      return {
+        ...o,
+        orderStatus: statuses.find((d) => d.id === o.orderStatusId).description,
+      };
     });
-
-    showOrderList("#order-list", fullOrders);
-  };
-  xhr2.send();
-};
-xhr.send();
+    showOrderList("#order-list", orders);
+  })
+  .catch((err) => showError("#order-list", err))
+  .finally(() => {
+    setTimeout(hideWaiting, 1500);
+  });
