08314d0807191f2523c6b5d999cd04e56a37d54a
Nate Tyalor <taylonr@gmail.com>
2022-12-29 08:54:48 -0600

    End of queuing promises

--- public/orders.js
+++ public/orders.js
@@ -1,24 +1,46 @@
-let statuses = [];
-showWaiting();
+let statusReq = axios.get("http://localhost:3000/api/orderStatuses");
+let addressReq = axios.get("http://localhost:3000/api/addresses");
+let addressTypeReq = axios.get("http://localhost:3000/api/addressTypes");
 
-axios
-  .get("http://localhost:3000/api/orderStatuses")
-  .then(({ data }) => {
-    statuses = data;
+let statuses = [];
+let addresses = [];
+let addressTypes = [];
+
+Promise.allSettled([statusReq, addressReq, addressTypeReq])
+  .then(([statusRes, addressRes, addressTypeRes]) => {
+    if (statusRes.status === "fulfilled") {
+      statuses = statusRes.value.data;
+    } else {
+      window.alert("Order status error: " + statusRes.reason.message);
+    }
+
+    if (addressRes.status === "fulfilled") {
+      addresses = addressRes.value.data;
+    } else {
+      window.alert("Addresses error: " + addressRes.reason.message);
+    }
+
+    if (addressTypeRes.status === "fulfilled") {
+      addressTypes = addressTypeRes.value.data;
+    } else {
+      window.alert("Address Type error: " + addressTypeRes.reason.message);
+    }
 
     return axios.get("http://localhost:3000/api/orders");
   })
   .then(({ data }) => {
-    let orders = data.map((o) => {
+    let orders = data.map((d) => {
+      const addr = addresses.find((a) => a.id === d.shippingAddress);
+
       return {
-        ...o,
-        orderStatus: statuses.find((d) => d.id === o.order.orderStatusId)
-          .description,
+        ...d,
+        orderStatus: statuses.find((s) => s.id === d.orderStatusId).description,
+        shippingAddressText: `${addr.street} ${addr.city}, ${addr.state} ${addr.zipCode}`,
       };
     });
+
     showOrderList("#order-list", orders);
   })
-  .catch((err) => showError("#order-list", err))
-  .finally(() => {
-    setTimeout(hideWaiting, 1500);
+  .catch((reasons) => {
+    showError("#order-list", reasons);
   });
